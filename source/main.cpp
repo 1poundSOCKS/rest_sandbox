0509d475a031d3f3094c1848650e5431075343cf
1poundSOCKS <mathew.coburn@gmail.com>
2025-04-12 15:35:06 +0100

    added asio listener

--- source/main.cpp
+++ source/main.cpp
@@ -2,5 +2,29 @@
 
 int main(int argc, char* argv[])
 {
+  try
+  {
+    auto const address =  boost::asio::ip::make_address("0.0.0.0");
+    unsigned short port = 8080;
+
+    boost::asio::io_context ioc{1};
+
+    boost::beast::error_code ec;
+    boost::asio::ip::tcp::acceptor acceptor(boost::asio::make_strand(ioc));
+
+    auto endpoint = boost::asio::ip::tcp::endpoint{address, port};
+
+    acceptor.open(endpoint.protocol(), ec);
+    if (ec)
+    {
+        std::cerr << "Open error: " << ec.message() << std::endl;
+        return;
+    }
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << "Error: " << e.what() << std::endl;
+  }
+
   return 0;
 }
