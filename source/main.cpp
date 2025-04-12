ebae8a2a4f474ae054ef02ff5dc04632057754c3
1poundSOCKS <mathew.coburn@gmail.com>
2025-04-12 16:22:10 +0100

    listener working

--- source/main.cpp
+++ source/main.cpp
@@ -5,25 +5,49 @@ int main(int argc, char* argv[])
   try
   {
     auto const address =  boost::asio::ip::make_address("0.0.0.0");
-    unsigned short port = 8080;
+    unsigned short port = 14571;
 
-    boost::asio::io_context ioc{1};
+    boost::asio::io_context ioc(1);
+    // ioc.run();
 
     boost::beast::error_code ec;
-    boost::asio::ip::tcp::acceptor acceptor(boost::asio::make_strand(ioc));
 
-    auto endpoint = boost::asio::ip::tcp::endpoint{address, port};
+    auto strand = boost::asio::make_strand(ioc);
+    boost::asio::ip::tcp::acceptor acceptor(strand);
+
+    auto endpoint = boost::asio::ip::tcp::endpoint(address, port);
 
     acceptor.open(endpoint.protocol(), ec);
     if (ec)
     {
-        std::cerr << "Open error: " << ec.message() << std::endl;
-        return;
+        std::cout << "Set option error: " << ec.message() << std::endl;
+        return 0;
+    }
+
+    acceptor.set_option(boost::asio::socket_base::reuse_address(true), ec);
+    if (ec)
+    {
+        std::cout << "Set option error: " << ec.message() << std::endl;
+        return 0;
+    }
+
+    acceptor.bind(endpoint, ec);
+    if (ec)
+    {
+        std::cout << "Bind error: " << ec.message() << std::endl;
+        return 0;
+    }
+
+    acceptor.listen(boost::asio::socket_base::max_listen_connections, ec);
+    if (ec)
+    {
+        std::cout  << "Listen error: " << ec.message() << std::endl;
+        return 0;
     }
   }
   catch (const std::exception& e)
   {
-    std::cerr << "Error: " << e.what() << std::endl;
+    std::cout << "Error: " << e.what() << std::endl;
   }
 
   return 0;
