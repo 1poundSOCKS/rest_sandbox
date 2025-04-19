54d92b14944de814b147ca5d5053cb23b3f9c750
1poundSOCKS <mathew.coburn@gmail.com>
2025-04-19 06:37:21 +0100

    makes a http call to httpbin.org

--- source/main.cpp
+++ source/main.cpp
@@ -165,6 +165,18 @@ void CallServer(boost::asio::io_context& ioc)
 
     boost::beast::tcp_stream stream(ioc);
     stream.connect(endpoint);
+
+    boost::beast::http::request<boost::beast::http::string_body> req{boost::beast::http::verb::get, "/ip", 10};
+    req.set(boost::beast::http::field::host, host);
+    req.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+    boost::beast::http::write(stream, req);
+
+    boost::beast::flat_buffer buffer;
+    boost::beast::http::response<boost::beast::http::dynamic_body> res;
+    boost::beast::http::read(stream, buffer, res);
+
+    std::cout << res << "\n";
   }
   catch( boost::system::system_error& err )
   {
