28a9c597f3e62d798fe0678f60609468f7ee6d2c
1poundSOCKS <mathew.coburn@gmail.com>
2025-05-16 19:49:29 +0100

    refactor

--- source/database.h
+++ source/database.h
@@ -15,47 +15,55 @@ public:
     std::string name;
   };
 
-  database(const char* connString) : m_conn(connString)
-  {
-    initialize();
-  }
-
-  inline std::string dbVersion(transaction& txn)
-  {
-    result r = txn.exec("SELECT version();");
-    return r[0][0].as<std::string>();
-  }
-
-  inline transaction startTransaction()
-  {
-    return transaction(m_conn);
-  }
-
-  inline int getMaxJobId(transaction& txn)
-  {
-    int maxId = -1;
-    database::result r = txn.exec_prepared("GET_MAX_JOB_ID");
-    for( auto row : r )
-    {
-      auto id = row["id"];
-      maxId = id.is_null() ? -1 : id.as<int>();
-    }
-    return maxId;
-  }
-  
-  inline void insert(transaction& txn, const jobs_record& record)
-  {
-      txn.exec_prepared("INSERT_JOB", record.transactionId, record.id, record.name);
-  }
+  inline database(const char* connString);
+  inline transaction startTransaction();
+  inline std::string dbVersion(transaction& txn);
+  inline int getMaxJobId(transaction& txn);
+  inline void insert(transaction& txn, const jobs_record& record);
 
 private:
 
-  inline void initialize()
-  {
-    m_conn.prepare("GET_MAX_JOB_ID", "SELECT MAX(id) as id FROM jobs");
-    m_conn.prepare("INSERT_JOB", "INSERT INTO jobs(transaction_id, id, name) VALUES ($1, $2, $3)");
-  }
+  inline void initialize();
 
   connection m_conn;
 
 };
+
+inline database::database(const char* connString) : m_conn(connString)
+{
+  initialize();
+}
+
+inline database::transaction database::startTransaction()
+{
+  return transaction(m_conn);
+}
+
+inline std::string database::dbVersion(transaction& txn)
+{
+  result r = txn.exec("SELECT version();");
+  return r[0][0].as<std::string>();
+}
+
+inline int database::getMaxJobId(transaction& txn)
+{
+  int maxId = -1;
+  database::result r = txn.exec_prepared("GET_MAX_JOB_ID");
+  for( auto row : r )
+  {
+    auto id = row["id"];
+    maxId = id.is_null() ? -1 : id.as<int>();
+  }
+  return maxId;
+}
+
+inline void database::insert(transaction& txn, const jobs_record& record)
+{
+    txn.exec_prepared("INSERT_JOB", record.transactionId, record.id, record.name);
+}
+
+inline void database::initialize()
+{
+  m_conn.prepare("GET_MAX_JOB_ID", "SELECT MAX(id) as id FROM jobs");
+  m_conn.prepare("INSERT_JOB", "INSERT INTO jobs(transaction_id, id, name) VALUES ($1, $2, $3)");
+}
